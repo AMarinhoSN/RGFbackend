6c468bc8a56c8c04813ba3b2220013645496c775
AMarinhoSN <amarinhosn@pm.me>
2021-08-05 20:52:49 -0300

    [enh] local pkg renamed to watchers (avoid confusion with the watchdogs)

--- watchers/logger.py
+++ watchers/logger.py
@@ -66,4 +66,4 @@ if __name__ == "__main__":
             time.sleep(1)
     except KeyboardInterrupt:
         log_watchdog.deactivate()
-    print(" WATCHDOG WAS MURDERED... by a keyboard interruption.")
+        print(" WATCHDOG WAS MURDERED... by a keyboard interruption.")
